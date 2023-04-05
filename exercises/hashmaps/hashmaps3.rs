d5ebb8cfe72236a295066d5d61a01f4963c006d6
0130w <1157374677@qq.com>
2023-04-06 00:57:58 +0800

    update

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -40,6 +38,18 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        let mut temp = scores.entry(team_1_name)
+        .or_insert(Team{name : v[0].to_string(), goals_scored : 0, goals_conceded : 0});
+        (*temp).goals_scored += team_1_score;
+        (*temp).goals_conceded += team_2_score;
+
+        let mut temp = scores.entry(team_2_name)
+        .or_insert(Team{name : v[1].to_string(), goals_scored : 0, goals_conceded : 0});
+        (*temp).goals_scored += team_2_score;
+        (*temp).goals_conceded += team_1_score;
+
+
+
     }
     scores
 }
--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -1,18 +1,18 @@
 // options3.rs
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
 }
 
-fn main() {
-    let y: Option<Point> = Some(Point { x: 100, y: 200 });
-
+fn main(){
+    let y: Option<&Point> = Some(&Point { x: 100, y: 200 });
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        // 这相当是Some(value)然后把&value绑定到了q上
+        Some(p) => {
+            println!("Co-ordinates are {},{} ", (*p).x, (*p).y)
+        }
         _ => println!("no match"),
     }
     y; // Fix without deleting this line.
